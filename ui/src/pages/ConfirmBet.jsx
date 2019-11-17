9b02a52ab4333b7e95aa1192818f46b90e26c301
josh <jhell85@gmail.com>
2019-11-17 09:59:40 -0800

    added images

--- ui/src/pages/ConfirmBet.jsx
+++ ui/src/pages/ConfirmBet.jsx
@@ -3,19 +3,45 @@ import client from "../api/client";
 import { Redirect } from "react-router-dom";
 
 const ConfirmBetForm = () => {
-  const { 0: confirmBet } = useGlobal("confirmBet");
   const { 0: token } = useGlobal("token");
+  const { 0: confirmBet } = useGlobal("confirmBet");
+
+  const gameInfo = () => {
+    return (
+      <div>
+        <div>NFL week {confirmBet.game.week}</div>
+        <div>
+          {confirmBet.homeReferences.city} {confirmBet.homeReferences.name}
+        </div>
+        <div>@</div>
+        <div>
+          {confirmBet.awayReferences.city} {confirmBet.awayReferences.name}
+        </div>
+      </div>
+    );
+  };
+  const payOutInfo = () => {
+    return (
+      <div>
+        <div>wagering: {confirmBet.wager}</div>
+        <div>
+          if you win you will collect {confirmBet.payOut} from your opponent
+        </div>
+        <div>
+          if you lose you will owe {confirmBet.accepterPayOut} to your opponent
+        </div>
+      </div>
+    );
+  };
   const betParameters = () => {
+    console.log(confirmBet);
     if (confirmBet.betType === "overUnder")
       if (confirmBet.homeAway === true)
         return (
           <div className="center-wrapper">
             <div className="text">
-              total combined points to be over {confirmBet.betTypeData.overUnder.overUnder}{" "}
-              points
-            </div>
-            <div>
-              wagering: {confirmBet.wager} to win: {confirmBet.payOut}
+              betting that the total combined points of the game to be over{" "}
+              {confirmBet.betTypeData.overUnder.overUnder} points
             </div>
           </div>
         );
@@ -23,27 +49,33 @@ const ConfirmBetForm = () => {
         return (
           <div className="center-wrapper">
             <div className="text">
-              total combined points to be under {confirmBet.betTypeData.overUnder.overUnder}{" "}
-              points
-            </div>
-            <div>
-              wagering: {confirmBet.wager} to win: {confirmBet.payOut}
+              betting that the total combined points to be under{" "}
+              {confirmBet.betTypeData.overUnder.overUnder} points
             </div>
           </div>
         );
     else if (confirmBet.betType === "pointSpread")
       if (confirmBet.homeAway === true)
-        return (
-          <div className="center-wrapper">
-            <div className="text">
-              {confirmBet.homeReferences.city} {confirmBet.homeReferences.name}{" "}
-              at home {confirmBet.betTypeData.pointSpread.homeSpread}
+        if (confirmBet.betTypeData.pointSpread.homeSpread > 0)
+          return (
+            <div className="center-wrapper">
+              <div className="text">
+                betting that the {confirmBet.homeReferences.city}{" "}
+                {confirmBet.homeReferences.name} will win by{" "}
+                {confirmBet.betTypeData.pointSpread.homeSpread} points or more.
+              </div>
             </div>
-            <div>
-              wagering: {confirmBet.wager} to win: {confirmBet.payOut}
+          );
+        else
+          return (
+            <div className="center-wrapper">
+              <div className="text">
+                betting that the {confirmBet.homeReferences.city}{" "}
+                {confirmBet.homeReferences.name} will not lose by more then{" "}
+                {confirmBet.betTypeData.pointSpread.homeSpread} points.
+              </div>
             </div>
-          </div>
-        );
+          );
       else
         return (
           <div className="center-wrapper">
@@ -99,17 +131,19 @@ const ConfirmBetForm = () => {
   };
   return (
     <>
-    {betSelected && <Redirect to="/profile" />}
-    <div className="center-wrapper">
-      <div>
-        <h1>{betParameters()}</h1>
+      {betSelected && <Redirect to="/profile" />}
+      <div className="center-wrapper">
+        <div>{gameInfo()}</div>
+        <div>
+          <h1>{betParameters()}</h1>
+        </div>
+        <div>{payOutInfo()}</div>
+        <div>
+          <form onSubmit={postBet}>
+            <button>Create Bet</button>
+          </form>
+        </div>
       </div>
-      <div>
-        <form onSubmit={postBet}>
-          <button>Create Bet</button>
-        </form>
-      </div>
-    </div>
     </>
   );
 };
--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -1,12 +1,23 @@
 import React, { useGlobal } from "reactn";
-
+import betImage from "../images/sports-betting9.jpg"
+import angryTrump from "../images/angry-trump.jpg"
 const Home = () => {
   const { 0: token } = useGlobal("token");
-
+if(token === null)
   return (
     <div>
-      <h1>Home {JSON.stringify(token)}</h1>
+      <h1>Wager Placer</h1>
+      <img src={betImage} />
+      <img width="290px" height="200px" src={angryTrump}/>
     </div>
+  )
+  else
+  return (
+    <div> 
+    <h1>Wager Placer</h1>
+    <img src={betImage} />
+    <div >{JSON.stringify(token)}</div>
+  </div>
   )
 }
 
